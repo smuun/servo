c36fd9cf55c1194eb8f7ba5cea54fc709646efcf
smuun <72632230+smuun@users.noreply.github.com>
2024-04-29 00:20:45 -0400

    servo tester

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,87 @@
 #![no_std]
 #![no_main]
 
+use core::f32;
+
 use esp_backtrace as _;
-use esp_hal::{clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*};
-
-extern crate alloc;
-use core::mem::MaybeUninit;
-
-#[global_allocator]
-static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
-
-fn init_heap() {
-    const HEAP_SIZE: usize = 32 * 1024;
-    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
-
-    unsafe {
-        ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
-    }
-}
+use esp_hal::{
+    analog::adc::{AdcCalLine, AdcConfig, Attenuation, ADC},
+    clock::ClockControl, delay::Delay, gpio::{GpioPin, Output, PushPull, IO}, peripherals::{Peripherals, ADC1}, prelude::*, xtensa_lx::interrupt::get
+};
+use esp_println::println;
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
-
-    let clocks = ClockControl::max(system.clock_control).freeze();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
     let delay = Delay::new(&clocks);
-    init_heap();
 
-    esp_println::logger::init_logger_from_env();
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
-    let timer = esp_hal::timer::TimerGroup::new(peripherals.TIMG1, &clocks, None).timer0;
-    let _init = esp_wifi::initialize(
-        esp_wifi::EspWifiInitFor::Wifi,
-        timer,
-        esp_hal::rng::Rng::new(peripherals.RNG),
-        system.radio_clock_control,
-        &clocks,
-    )
-    .unwrap();
+
+    type AdcCal = AdcCalLine<ADC1>;
+    let pot_pin = io.pins.gpio1.into_analog();
+    let mut adc1_config = AdcConfig::<ADC1>::new();
+    let mut adc1_pin = adc1_config.enable_pin_with_cal::<_, AdcCal>(pot_pin, Attenuation::Attenuation11dB);
+    let mut adc1 = ADC::<ADC1>::new(peripherals.ADC1, adc1_config);
+
+    let mut highest = 3.41;
+    let mut lowest = 0.0;
+
+    println!("calibration -- turn knob through its range and click to accept");
+
+    let cal_pin = io.pins.gpio45.into_pull_down_input();
+
+    let mut cal = false;
+
+    let mut get_voltage = || {
+        nb::block!(adc1.read_oneshot(&mut adc1_pin)).unwrap() as f32 / 1000.00
+    };
+    while !cal {
+        let voltage = get_voltage(); 
+        cal = cal_pin.is_low();
+        if voltage > highest {
+            highest = voltage;
+        }
+        if voltage < lowest {
+            lowest = voltage;
+        }
+        delay.delay_millis(20 as u32);
+    }
+    println!("cal high {highest} low {lowest}");
+
+    let mut servo_pin = io.pins.gpio46.into_push_pull_output();
+    // nanoseconds
+    let period: u32 = 20_000_000;
+
+    let mut running_average: f32 = 0.0;
+
+    let mut get_on_off = || {
+        let tmp_angle = ((get_voltage() - lowest) / (highest - lowest)).clamp(0.0, 1.0);
+        let window = 5.0;
+        running_average = (running_average * window + tmp_angle) / (window + 1.0);
+        let angle = running_average;
+
+        let zero = 0.025;
+        let one_eighty = 0.125;
+        let duty_cycle = angle * (one_eighty - zero) + zero;
+        let on = (duty_cycle * period as f32) as u32;
+        // println!("dc {duty_cycle} on {on} / period {period}");
+        // delay.delay_millis(500);
+
+        let off: u32 = period - on;
+        // println!("ratio {ratio} \t dc {duty_cycle}", ratio = (on as f32 / period as f32));
+        (on, off)
+    };
+
 
     loop {
-        log::info!("Hello world!");
-        delay.delay(500.millis());
+        let (on, off) = get_on_off();
+        servo_pin.set_low();
+        delay.delay_nanos(off);
+        servo_pin.set_high();
+        delay.delay_nanos(on);
     }
+    
 }
