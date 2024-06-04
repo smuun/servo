d1b49b4a1f01dedfdac6973b91be08a333cb0aa2
smuun <72632230+smuun@users.noreply.github.com>
2024-06-03 21:45:23 -0400

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,22 @@
+//! Uses timer0 and operator0 of the MCPWM0 peripheral to output a 50% duty
+//! signal at 20 kHz.
+//!
+//! The signal will be output to the pin assigned to `pin`. (GPIO0)
+
+//% CHIPS: esp32 esp32c6 esp32h2 esp32s3
+
 #![no_std]
 #![no_main]
 
-use core::f32;
-
 use esp_backtrace as _;
 use esp_hal::{
-    analog::adc::{AdcCalLine, AdcConfig, Attenuation, ADC},
     clock::ClockControl,
-    delay::Delay,
     gpio::IO,
-    peripherals::{Peripherals, ADC1},
-    mcpwm,
+    mcpwm::{operator::PwmPinConfig, timer::PwmWorkingMode, PeripheralClockConfig, MCPWM},
+    peripherals::Peripherals,
     prelude::*,
 };
 use esp_println::println;
-
-type AdcCal = AdcCalLine<ADC1>;
-
-fn calibrate(
-    delay: &Delay,
-    cal_pin: esp_hal::gpio::GpioPin<esp_hal::gpio::Input<esp_hal::gpio::PullDown>, 45>,
-    get_voltage: &mut impl FnMut() -> f32,
-) -> (f32, f32) {
-    let mut highest = 3.41;
-    let mut lowest = 0.0;
-
-    println!("calibration -- turn knob through its range and click to accept");
-
-    while cal_pin.is_low() {
-        let voltage = get_voltage();
-        if voltage > highest {
-            highest = voltage;
-        }
-        if voltage < lowest {
-            lowest = voltage;
-        }
-        delay.delay_millis(20 as u32);
-    }
-    println!("cal high {highest} low {lowest}");
-
-    (highest, lowest)
-}
-
-fn get_on_off(
-    get_voltage: &mut impl FnMut() -> f32,
-    lowest: f32,
-    highest: f32,
-    running_average: f32,
-    period: u32,
-) -> (u32, u32, f32) {
-    let tmp_angle = ((get_voltage() - lowest) / (highest - lowest)).clamp(0.0, 1.0);
-    let window = 5.0;
-    let r_avg = (running_average * window + tmp_angle) / (window + 1.0);
-    let angle = running_average;
-
-    let zero = 0.025;
-    let one_eighty = 0.125;
-    let duty_cycle = angle * (one_eighty - zero) + zero;
-    let on = (duty_cycle * period as f32) as u32;
-
-    let off: u32 = period - on;
-    (on, off, r_avg)
-}
 
 #[entry]
 fn main() -> ! {
@@ -69,33 +24,33 @@ fn main() -> ! {
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    let delay = Delay::new(&clocks);
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let pin = io.pins.gpio0;
 
-    let mut adc1_config = AdcConfig::<ADC1>::new();
-    let pot_pin = io.pins.gpio1.into_analog();
-    let mut adc1_pin =
-        adc1_config.enable_pin_with_cal::<_, AdcCal>(pot_pin, Attenuation::Attenuation11dB);
-    let mut adc1 = ADC::<ADC1>::new(peripherals.ADC1, adc1_config);
+    // initialize peripheral
+    #[cfg(feature = "esp32h2")]
+    let clock_cfg = PeripheralClockConfig::with_frequency(&clocks, 40.MHz()).unwrap();
+    #[cfg(not(feature = "esp32h2"))]
+    let clock_cfg = PeripheralClockConfig::with_frequency(&clocks, 32.MHz()).unwrap();
 
-    let mut get_voltage = || nb::block!(adc1.read_oneshot(&mut adc1_pin)).unwrap() as f32 / 1000.00;
+    let mut mcpwm = MCPWM::new(peripherals.MCPWM0, clock_cfg);
 
-    let cal_pin = io.pins.gpio45.into_pull_down_input();
-    let (highest, lowest) = calibrate(&delay, cal_pin, &mut get_voltage);
+    // connect operator0 to timer0
+    mcpwm.operator0.set_timer(&mcpwm.timer0);
+    // connect operator0 to pin
+    let mut pwm_pin = mcpwm
+        .operator0
+        .with_pin_a(pin, PwmPinConfig::UP_ACTIVE_HIGH);
 
-    let mut servo_pin = io.pins.gpio46.into_push_pull_output();
-    // nanoseconds
-    let period: u32 = 20_000_000;
+    // start timer with timestamp values in the range of 0..=99 and a frequency of
+    // 20 kHz
+    let timer_clock_cfg = clock_cfg
+        .timer_clock_with_frequency(50, PwmWorkingMode::Increase, 1.MHz())
+        .unwrap();
+    mcpwm.timer0.start(timer_clock_cfg);
 
-    let mut running_average: f32 = 0.0;
+    pwm_pin.set_timestamp(2);
 
     loop {
-        let (on, off, r_avg) =
-            get_on_off(&mut get_voltage, lowest, highest, running_average, period);
-        running_average = r_avg;
-        servo_pin.set_low();
-        delay.delay_nanos(off);
-        servo_pin.set_high();
-        delay.delay_nanos(on);
     }
 }
