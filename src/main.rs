b2b0678a443cd21a2e063a214934256e1b497a8f
smuun <72632230+smuun@users.noreply.github.com>
2024-07-14 14:46:39 -0400

    wrap everything in critical section

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
 use core::ops::Add;
+use critical_section::Mutex;
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
@@ -47,8 +49,6 @@ fn calibrated(raw: ArmCoordinates) -> ArmCoordinates {
     raw + ARM_ZERO
 }
 
-// unit distance is hundredths of arm length (70mm)
-// so the total possible range is at a radius 200 from the base
 #[derive(Debug, Copy, Clone, PartialEq)]
 struct CartesianCoordinates {
     x: Distance,
@@ -111,6 +111,14 @@ fn solve(target: &CartesianCoordinates) -> ArmCoordinates {
     ArmCoordinates { phi, alpha, beta }
 }
 
+enum Direction {
+    Go,
+    Return,
+}
+
+static SYSTEM: Mutex<RefCell<Option<esp_hal::system::SystemControl>>> =
+    Mutex::new(RefCell::new(None));
+
 fn get_pulse(angle: Angle) -> u16 {
     let bounded_angle = if 0.0 > angle {
         println!("clamping {angle} to 0");
@@ -132,125 +140,124 @@ fn get_pulse(angle: Angle) -> u16 {
 
 #[entry]
 fn main() -> ! {
-    let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    critical_section::with(|cs| {
+        let peripherals = Peripherals::take();
+        let system = SystemControl::new(peripherals.SYSTEM);
 
-    let delay = Delay::new(&clocks);
+        SYSTEM.borrow_ref_mut(cs).replace(system);
 
-    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    let pin0 = io.pins.gpio7;
-    let pin1 = io.pins.gpio6;
-    let pin2 = io.pins.gpio5;
+        let mut binding = SYSTEM.borrow_ref_mut(cs);
+        let mut tmp = binding.as_mut().unwrap();
 
-    let clock_cfg = PeripheralClockConfig::with_prescaler(&clocks, u8::MAX / 35);
-    // let clock_cfg = PeripheralClockConfig::with_frequency(&clocks, 49.Hz()).unwrap();
+        let clocks = ClockControl::boot_defaults(&mut tmp.clock_control).freeze();
+        let delay = Delay::new(&clocks);
+        let clock_cfg = PeripheralClockConfig::with_prescaler(&clocks, u8::MAX / 35);
+        dbg!(clock_cfg.frequency());
 
-    dbg!(clock_cfg.frequency());
+        let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+        let pin0 = io.pins.gpio7;
+        let pin1 = io.pins.gpio6;
+        let pin2 = io.pins.gpio5;
 
-    let mut mcpwm = McPwm::new(peripherals.MCPWM0, clock_cfg);
+        let mut mcpwm = McPwm::new(peripherals.MCPWM0, clock_cfg);
 
-    mcpwm.operator0.set_timer(&mcpwm.timer0);
-    let mut beta = mcpwm
-        .operator0
-        .with_pin_a(pin0, PwmPinConfig::UP_ACTIVE_HIGH);
+        mcpwm.operator0.set_timer(&mcpwm.timer0);
+        let mut beta = mcpwm
+            .operator0
+            .with_pin_a(pin0, PwmPinConfig::UP_ACTIVE_HIGH);
 
-    mcpwm.operator1.set_timer(&mcpwm.timer1);
-    let mut alpha = mcpwm
-        .operator1
-        .with_pin_a(pin1, PwmPinConfig::UP_ACTIVE_HIGH);
+        mcpwm.operator1.set_timer(&mcpwm.timer1);
+        let mut alpha = mcpwm
+            .operator1
+            .with_pin_a(pin1, PwmPinConfig::UP_ACTIVE_HIGH);
 
-    mcpwm.operator2.set_timer(&mcpwm.timer2);
-    let mut phi = mcpwm
-        .operator2
-        .with_pin_a(pin2, PwmPinConfig::UP_ACTIVE_HIGH);
+        mcpwm.operator2.set_timer(&mcpwm.timer2);
+        let mut phi = mcpwm
+            .operator2
+            .with_pin_a(pin2, PwmPinConfig::UP_ACTIVE_HIGH);
 
-    let timer_clock_cfg = clock_cfg
-        .timer_clock_with_frequency(2000, PwmWorkingMode::Increase, 50.Hz())
-        .unwrap();
+        let timer_clock_cfg = clock_cfg
+            .timer_clock_with_frequency(2000, PwmWorkingMode::Increase, 50.Hz())
+            .unwrap();
 
-    println!("timer frequency {fq}", fq = timer_clock_cfg.frequency());
-    mcpwm.timer0.start(timer_clock_cfg);
-    mcpwm.timer1.start(timer_clock_cfg);
-    mcpwm.timer2.start(timer_clock_cfg);
+        println!("timer frequency {fq}", fq = timer_clock_cfg.frequency());
+        mcpwm.timer0.start(timer_clock_cfg);
+        mcpwm.timer1.start(timer_clock_cfg);
+        mcpwm.timer2.start(timer_clock_cfg);
 
-    enum Direction {
-        Go,
-        Return,
-    }
+        let safe_travel = calibrated(ArmCoordinates {
+            phi: 0.0,
+            alpha: 90.0,
+            beta: 0.0,
+        });
 
-    let safe_travel = calibrated(ArmCoordinates {
-        phi: 0.0,
-        alpha: 90.0,
-        beta: 0.0,
-    });
+        let mut set_timestamps = |coordinates: &ArmCoordinates, direction: Direction| {
+            match direction {
+                Direction::Go => {
+                    beta.set_timestamp(get_pulse(coordinates.beta));
+                    delay.delay_millis(200);
+                    alpha.set_timestamp(get_pulse(coordinates.alpha));
+                    phi.set_timestamp(get_pulse(coordinates.phi));
+                }
 
-    let mut set_timestamps = |coordinates: &ArmCoordinates, direction: Direction| {
-        match direction {
-            Direction::Go => {
-                beta.set_timestamp(get_pulse(coordinates.beta));
-                delay.delay_millis(200);
-                alpha.set_timestamp(get_pulse(coordinates.alpha));
-                phi.set_timestamp(get_pulse(coordinates.phi));
-            }
+                Direction::Return => {
+                    alpha.set_timestamp(get_pulse(coordinates.alpha));
+                    delay.delay_millis(200);
+                    beta.set_timestamp(get_pulse(coordinates.beta));
+                    phi.set_timestamp(get_pulse(coordinates.phi));
+                }
+            };
+        };
 
-            Direction::Return => {
-                alpha.set_timestamp(get_pulse(coordinates.alpha));
-                delay.delay_millis(200);
-                beta.set_timestamp(get_pulse(coordinates.beta));
-                phi.set_timestamp(get_pulse(coordinates.phi));
+        let mut travel = |coordinates: &ArmCoordinates| {
+            set_timestamps(&safe_travel, Direction::Return);
+            delay.delay_millis(500);
+            set_timestamps(coordinates, Direction::Go);
+        };
+
+        let mut run_tests = || {
+            let test_points = [
+                CartesianCoordinates {
+                    x: 0.,
+                    y: 0.,
+                    z: 0.,
+                },
+                CartesianCoordinates {
+                    x: 0.99,
+                    y: 0.,
+                    z: 0.,
+                },
+                CartesianCoordinates {
+                    x: 100.,
+                    y: 0.,
+                    z: 0.,
+                },
+                CartesianCoordinates {
+                    x: 0.,
+                    y: 0.99,
+                    z: 0.,
+                },
+                CartesianCoordinates {
+                    x: 0.,
+                    y: 0.,
+                    z: 0.99,
+                },
+                CartesianCoordinates {
+                    x: 0.576,
+                    y: 0.576,
+                    z: 0.576,
+                },
+            ];
+            for target_cartesian in test_points {
+                delay.delay_millis(1_000);
+                dbg!(target_cartesian);
+                let target_arm = solve(&target_cartesian);
+                dbg!(target_arm);
+                travel(&calibrated(target_arm));
             }
         };
-    };
-
-    let mut travel = |coordinates: &ArmCoordinates| {
-        set_timestamps(&safe_travel, Direction::Return);
-        delay.delay_millis(500);
-        set_timestamps(coordinates, Direction::Go);
-    };
-
-    let mut run_tests = || {
-        let test_points = [
-            CartesianCoordinates {
-                x: 0.,
-                y: 0.,
-                z: 0.,
-            },
-            CartesianCoordinates {
-                x: 0.99,
-                y: 0.,
-                z: 0.,
-            },
-            CartesianCoordinates {
-                x: 100.,
-                y: 0.,
-                z: 0.,
-            },
-            CartesianCoordinates {
-                x: 0.,
-                y: 0.99,
-                z: 0.,
-            },
-            CartesianCoordinates {
-                x: 0.,
-                y: 0.,
-                z: 0.99,
-            },
-            CartesianCoordinates {
-                x: 0.576,
-                y: 0.576,
-                z: 0.576,
-            },
-        ];
-        for target_cartesian in test_points {
-            delay.delay_millis(1_000);
-            dbg!(target_cartesian);
-            let target_arm = solve(&target_cartesian);
-            dbg!(target_arm);
-            travel(&calibrated(target_arm));
-        }
-    };
-    run_tests();
+        run_tests();
+    });
 
     loop {}
 }
